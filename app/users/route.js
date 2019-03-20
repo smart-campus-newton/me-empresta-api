d09f02a3edb99008a933a04071ba859f2ed34101
Lucas Germano <lgermano@AC-000510-lgermano.local>
2019-03-20 11:54:51 -0300

    fix: put the secret on config file

--- app/users/route.js
+++ app/users/route.js
@@ -2,10 +2,10 @@ const express = require('express')
 const user = require('./controller')
 const jwt = require('jsonwebtoken')
 const auth = require('./../auth')
-const jwtSecret = 'DevPlenoRocks!'
+const config = require('config')
 
 const router = express.Router()
-
+const jwtSecret = config.get('APP.SECRET')
 router.post('/login', user.login({ jwt, jwtSecret }))
 
 router.route('/')
