ffc7449446fa335bec4e51ef181466b53bc5c2b6
Lucas Germano <lucas.germano@gmail.com>
2019-04-01 20:27:17 -0300

    feature: removing old folders and controllers

--- app/server.js
+++ app/server.js
@@ -9,10 +9,6 @@ const MongoAdapter = require('./adapter/mongo');
 
 const userRoutes = require('./users/route')
 const coursesRoutes = require('./courses/route')
-const shiftRoutes = require('./shift/route')
-const phoneRoutes = require('./phone/route')
-const statusRoutes = require('./status/route')
-const addressRoutes = require('./address/route')
 
 const app = express()
 const mongoAdapter = MongoAdapter()
@@ -26,10 +22,6 @@ app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 app.use(swStats.getMiddleware({ swaggerSpec: swaggerDocument }))
 app.use('/api/user', userRoutes)
 app.use('/api/courses', coursesRoutes)
-app.use('/api/shift', shiftRoutes)
-app.use('/api/phone', phoneRoutes)
-app.use('/api/status', statusRoutes)
-app.use('/api/address', addressRoutes)
 
 const upServer = () => {
   mongoAdapter.connect()
